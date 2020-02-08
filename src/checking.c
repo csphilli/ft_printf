60821bf0428cf64bb3493c9cd4809458002ffcc6
cphillip <cphillip@student.hive.fi>
2020-02-08 18:05:42 +0200

    additionl work on %x

--- src/checking.c
+++ src/checking.c
@@ -6,7 +6,7 @@
 /*   By: cphillip <cphillip@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/13 13:16:22 by cphillip          #+#    #+#             */
-/*   Updated: 2020/02/06 11:26:55 by cphillip         ###   ########.fr       */
+/*   Updated: 2020/02/08 10:25:42 by cphillip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ t_struct	*chk_conv_flags(t_struct *csp)
 	{
 		while (csp->format_flags[i] == csp->srch_fmt[csp->i])
 		{
+			
 			while (csp->srch_fmt[csp->i] == '-' && csp->i++)
 				csp->conv_flags[0] = '-';
 			while (csp->srch_fmt[csp->i] == '+' && csp->i++)
--- src/print_x.c
+++ src/print_x.c
@@ -6,11 +6,14 @@
 /*   By: cphillip <cphillip@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/26 11:49:10 by cphillip          #+#    #+#             */
-/*   Updated: 2020/02/07 18:47:49 by cphillip         ###   ########.fr       */
+/*   Updated: 2020/02/08 18:02:12 by cphillip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
+
+t_struct	*print_zero(t_struct *csp, char spec);
+
 
 static uintmax_t	get_nbr(t_struct *csp)
 {
@@ -54,17 +57,78 @@ t_struct	*print_x(t_struct *csp)
 	char		*tmp;
 	int			s_len;
 	uintmax_t	nbr;
-	int	zeroes;
-	int	mod;
-	//int	padding;
-	int left;
+	int			left;
+	int		filler;
+	int	u_l;
 
+	u_l = 0;
+
+	filler = 0;
+	left = 0;
+	nbr = get_nbr(csp);
+	tmp = convert_nbr(nbr, 16);
+	if (nbr == 0 && csp->precision == 0)
+	{
+		print_alt(csp, csp->width, ' ');
+		return (csp);
+	}
+	if (csp->conv_flags[0] == '-')
+		left = 1;
+	s_len = ft_strlen(tmp);
+	//printf("slenA:%d\n", s_len);
 	
+	if (csp->conv_flags[4] == '0' && csp->conv_flags[3] == '#' && csp->width && nbr)
+		u_l = s_len + 2;
+	else if (csp->conv_flags[3] == '#' && nbr)
+		csp->width -= 2;
+	//printf("slenB:%d\n", s_len);
+	//printf("width:%ld\n", csp->width);
+	//printf("precA:%ld\n", csp->precision);
+	if (!left && csp->conv_flags[4] == '0' && csp->precision == -1)
+		csp->precision = csp->width;
+	//printf("precB:%ld\n", csp->precision);
+	filler = (u_l <= csp->precision && csp->precision > 0) ? csp->precision : u_l;
+
+	//printf("s_len:%d\n", s_len);
+	//printf("filler:%d\n", filler);
+	if (!left)
+		print_alt(csp, csp->width - filler, ' ');
+	print_zero(csp, csp->specifier);
+	print_alt(csp, csp->precision - u_l, '0');
+	ft_putstr(tmp);
+	if (left)
+		print_alt(csp, csp->width - filler, ' ');
+	
+	return (csp);
+}
+
+t_struct	*print_zero(t_struct *csp, char spec)
+{
+	if (csp->conv_flags[3] == '#')
+	{
+		if (spec == 'X')
+			write(1, "0X", 2);
+		if (spec == 'x')
+			write(1, "0x", 2);
+	}
+	csp->len += 2;
+	return(csp);
+}
+
+/*
+	
+
+// 2ND ITERATION
+
+l_n = 0;
+
+	t_len = 0;
 	left = 0; // 0 == right; 1 == left
 	nbr = get_nbr(csp);
 	tmp = convert_nbr(nbr, 16);
 	mod = 0;
 	zeroes = 0;
+	
 	if (nbr == 0 && csp->precision == 0)
 	{
 		print_alt(csp, csp->width, ' ');
@@ -77,20 +141,28 @@ t_struct	*print_x(t_struct *csp)
 	if (csp->specifier == 'X')
 		tmp = ft_s_toupper(tmp);
 	s_len = ft_strlen(tmp);
+	printf("conv[0]:%c\n", csp->conv_flags[0]);
+	printf("csp->prec:%ld\n", csp->precision);
+	printf("s_len:%d\n", s_len);
+	printf("mod:%d\n", mod);
+
+	//if (csp->conv_flags[4] && !csp->conv_flags[0] && !csp->precision)
+		//csp->precision = csp->width;
 	
-
-	//printf("csp->prec:%ld\n", csp->precision);
-	//printf("s_len:%d\n", s_len);
 	if (csp->conv_flags[4] && !csp->precision)
-		zeroes = csp->width - s_len;
+		zeroes = csp->width - s_len - mod;
 	else if ((!csp->conv_flags[4] || csp->conv_flags[4]) && csp->precision > s_len)
-		zeroes = csp->precision - s_len;
-	//else if (csp->conv_flags[4] && csp->precision > s_len)
-		//zeroes = csp->precision - s_len;
-	//printf("zeroes:%d\n", zeroes);
-
+		zeroes = csp->precision - s_len - mod;
+	printf("zeroes:%d\n", zeroes);
+	t_len = csp->width - zeroes - s_len - mod;
+	printf("t_len:%d\n", t_len);
+	printf("left:%d\n", left);
+	printf("\n:");
+	
 	if (!left && !csp->conv_flags[4])
-		print_alt(csp, csp->width - zeroes - s_len - mod, ' ');
+		print_alt(csp, csp->width - zeroes - s_len, ' ');
+	else if (!left && csp->conv_flags[4] && csp->precision < s_len)
+		print_alt(csp, t_len, ' ');
 
 	//else if (!csp->conv_flags[4] && !align)
 		//print_alt(csp, csp->width - zeroes - s_len - mod, ' ');
@@ -98,18 +170,13 @@ t_struct	*print_x(t_struct *csp)
 		
 	if (csp->conv_flags[3])
 		write(1, "0x", 2);
-	if (csp->conv_flags[4] && csp->precision == -1)
-		print_alt(csp, csp->width - s_len, '0');
+	if (csp->conv_flags[4] && (csp->precision == -1 || (csp->precision && csp->precision < s_len)))
+		print_alt(csp, zeroes, '0');
+	else if (csp->conv_flags[4] && csp->precision && csp->precision < s_len)
+		print_alt(csp, t_len, ' ');
 	else
 		print_alt(csp, zeroes, '0');
-	/*
-	if (csp->conv_flags[4] && csp->precision == -1)
-		print_alt(csp, csp->width - s_len, '0');
-	else if (csp->precision && csp->precision > s_len && !csp->conv_flags[4])
-		print_alt(csp, csp->precision - s_len, '0');
-	else if (csp->conv_flags[4] && !csp->precision)
-		print_alt(csp, csp->width - s_len, '0');
-	*/
+	
 	ft_putstr(tmp);
 	if (left)
 		print_alt(csp, csp->width - zeroes - s_len - mod, ' ');
@@ -117,9 +184,48 @@ t_struct	*print_x(t_struct *csp)
 
 		
 	csp->len += s_len;
-	//padding = get_padding(csp, s_len);
-	//align_print(csp, tmp, s_len);
-	return (csp);
-}
 
+
+THIRD ITERATION -- GETTING FRUSTRATED
+
+
+	if (csp->precision != -1 && csp->precision > s_len)
+		m_z = csp->precision - s_len;
+	if (csp->width == 0 && csp->precision > -1)
+		filler = 0;
+	else
+		filler = csp->width - m_z - s_len - mod;
+	//printf("filler:%d\n", filler);
+	//printf("\n:");
+	//printf("left:%d\n", left);
+// im off where:
+
+	//printf("lz:%d\n", filler);
+// Start off by printing a hex value if 0 and #
+	if (!left)
+		print_alt(csp, csp->width - )
+	if (csp->conv_flags[3] && m_z == 0)
+		write(1, "0x", 2);
+	if (!left && csp->conv_flags[4] == '0' && csp->precision == -1)
+		print_alt(csp, filler, '0');
+
+// Next, print either the zeros or empty space.
+	if (csp->conv_flags[3] && m_z != 0)
+		write(1, "0x", 2);
+	
+
+// Now if there wasn't 0 but #, print that.
+
+	if (m_z > 0)
+		print_alt(csp, m_z, '0');
+	
+	ft_putstr(tmp);
+
+	if (csp->conv_flags[0] == '-')
+		print_alt(csp, filler, ' ');
+	
+	//printf("s_len:%d\n", s_len);
+	//printf("\n:");
+	
+	*/
 
