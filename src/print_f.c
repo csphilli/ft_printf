ac0b25bf3900ce2fa16d0c9e605b326e93d2b593
cphillip <cphillip@student.hive.fi>
2020-02-28 13:28:17 +0200

    adding in formatting for %f

--- src/print_f.c
+++ src/print_f.c
@@ -6,7 +6,7 @@
 /*   By: cphillip <cphillip@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/12 10:15:00 by cphillip          #+#    #+#             */
-/*   Updated: 2020/02/27 15:24:14 by cphillip         ###   ########.fr       */
+/*   Updated: 2020/02/28 13:25:50 by cphillip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,7 @@ char		*get_decimal(long double nbr, int prec, char dot)
 */
 static long double	rounding(long double nbr, int prec)
 {
-	long double	rounding;
+	long double rounding;
 	int			i;
 
 	i = 0;
@@ -76,31 +76,32 @@ static long double	rounding(long double nbr, int prec)
 
 char		*ft_ftoa(long double nbr, int prec, char dot)
 {
-	char	*joint;
-	char 	*whole;
+	char				*joint;
+	char 				*whole;
 	char				*dec_part;
-	unsigned long long dec;
+	unsigned long long 	dec;
 	int i;
 
 	i = 0;
-	nbr += rounding(nbr, prec);
+	prec = prec == -1 ? 6 : prec;
+	nbr += prec > 0 ? rounding(nbr, prec) : 0;
 	nbr < 0 ? nbr *= -1 : nbr * 1;
 	whole = ft_itoa_uintmax(nbr);
 	dec = nbr;
 	nbr = prec > 0 ? nbr - dec : 0;
 	dec_part = ft_strnew(prec + 1);
-	dec_part[i++] = dot;
+	dec_part[i++] = (dot && prec > 0) ? '.' : '\0';
 	while (prec-- > 0)
-	{
-		nbr *= 10;
-		dec = nbr;
-		nbr -= dec;
-		dec_part[i++] = dec + '0'; 
-	}
+		{
+			nbr *= 10;
+			dec = nbr;
+			nbr -= dec;
+			dec_part[i++] = dec + '0';
+		}
 	joint = ft_strjoin(whole, dec_part);
 	free(whole);
 	free(dec_part);
-	return(joint);	
+	return(joint);
 }
 
 char	*ft_itoa_uintmax(uintmax_t n)
@@ -128,13 +129,41 @@ t_struct			*print_f(t_struct *csp)
 {
 	long double	nbr;
 	char		*flt;
-	int		not_blank;
-	// char		*dec;
+	int			not_blank;
+	int			blank;
 
 	nbr = get_float(csp);
 	flt = ft_ftoa(nbr, csp->prec, '.');
 	not_blank = ft_strlen(flt);
-	printf("n_blank:%d", not_blank);
+	if (nbr < 0 && csp->width > not_blank)
+		not_blank++;
+	blank = csp->width - not_blank;
+	
+	if (csp->c_flags[1] == '+' || csp->c_flags[2] == ' ')
+		blank--;
 
+// IF ZEROES
+	// printf("blank:%d\n", blank);
+	// Look at nbr 86 and 87. They are similar in input yet one impacts the other differently.
+
+	if (!csp->c_flags[0] && !csp->c_flags[4])
+		print_alt(csp, csp->width - not_blank, ' ');
+
+	//I have to also print the ' '. So perhaps that can be a separate function?
+	if (nbr < 0)
+		print_alt(csp, 1, '-');
+	if (nbr >= 0 && csp->c_flags[1] == '+')
+		print_alt(csp, 1, '+');
+	
+	if (csp->c_flags[4] == '0' && blank > 0 && csp->prec <= 0)
+		print_alt(csp, blank, '0');
+	
+
+	ft_putstr(flt);
+	if (csp->c_flags[3] && csp->prec == 0)
+		print_alt(csp, 1, '.');
+	if (csp->c_flags[0] == '-')
+		print_alt(csp, blank, ' ');
+	csp->len += not_blank;
 	return (csp);
 }
