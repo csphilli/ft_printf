0e67f7341f12ad59bce8135b7757cdf1bd5ae292
csphilli <csphilli@gmail.com>
2020-03-09 11:12:13 +0200

    fixed %d

--- src/print_d.c
+++ src/print_d.c
@@ -6,7 +6,7 @@
 /*   By: csphilli <csphilli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/12 10:15:00 by cphillip          #+#    #+#             */
-/*   Updated: 2020/03/09 00:02:08 by csphilli         ###   ########.fr       */
+/*   Updated: 2020/03/09 11:11:30 by csphilli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,24 +43,6 @@ void		do_basic_d(t_struct *csp, intmax_t nbr)
 	}
 }
 
-// static int	collect_d(t_struct *csp, int is_neg)
-// {
-// 	int n_blank;
-
-// 	n_blank = csp->s_len;
-// 	if (csp->c_flags[4] == '0' && csp->prec == -1 && csp->c_flags[3] != '-')
-// 	{
-// 		csp->prec = csp->width;
-// 		if (is_neg || csp->c_flags[1] == '+' || csp->c_flags[2] == ' ')
-// 			csp->prec--;
-// 	}
-// 	if (csp->s_len <= csp->prec && csp->prec > 0)
-// 		n_blank = csp->prec;
-// 	if (is_neg || csp->get_plus != '\0')
-// 		n_blank++;
-// 	return (n_blank);
-// }
-
 void		do_d(t_struct *csp, int zeroes, int is_neg)
 {
 	if (csp->c_flags[0] != '-')
@@ -77,26 +59,18 @@ int		get_zeroes(t_struct *csp, int is_neg)
 	int			zeroes;
 
 	zeroes = 0;
-	printf("\ns_len:%d\n", csp->s_len);
 	if (csp->c_flags[4] == '0' && csp->c_flags[0] != '-')
 	{
 		if (csp->prec >= 0 && csp->prec >= csp->s_len)
-		{
-			printf("\nFIRST\n");
 			zeroes = csp->prec - csp->s_len;
-		}
 		else if (csp->prec >= 0 && csp->prec < csp->s_len)
-		{
-			printf("\nSECOND\n");
 			zeroes = 0;
-		}
 		else if (csp->prec == -1)
 		{
-			printf("\nTHIRD\n");
-			csp->s_len += is_neg ? 1 : 0;
-			csp->s_len += csp->c_flags[2] == ' ' ? 1 : 0;
 			zeroes = csp->width - csp->s_len;
-			printf("zeroes THIRD:%d\n", zeroes);
+			zeroes -= is_neg ? 1 : 0;
+			if (csp->c_flags[2] == ' ' && csp->c_flags[1] != '+' && !is_neg)
+				zeroes -= 1;
 		}
 		if (csp->c_flags[1] == '+' && !is_neg && csp->prec == -1)
 			zeroes -= 1;
@@ -106,7 +80,6 @@ int		get_zeroes(t_struct *csp, int is_neg)
 		if (csp->prec > 0 && csp->prec > csp->s_len)
 			zeroes = csp->prec - csp->s_len;
 	}
-	printf("zeroes FINAL:%d\n", zeroes);
 	return (zeroes);
 }
 
@@ -126,12 +99,9 @@ t_struct	*print_d(t_struct *csp)
 	is_neg = (nbr < 0) ? 1 : 0;
 	csp->s_len = nbr == 0 ? 1 : ft_nbr_size(nbr);
 	nbr *= (is_neg && nbr != (-9223372036854775807 -1)) ? -1 : 1;
-	// csp->s_len += is_neg ? 1 : 0;
 	zeroes = get_zeroes(csp, is_neg);
-	csp->s_len += csp->get_plus != '\0' || is_neg ? 1 : 0;
-	
-	do_d(csp, zeroes, is_neg);
-	
+	csp->s_len += csp->get_plus != '\0' || is_neg ? 1 : 0;	
+	do_d(csp, zeroes, is_neg);	
 	ft_putnbrmax(nbr);	
 	if (csp->c_flags[0] == '-')
 		print_alt(csp, csp->width - zeroes - csp->s_len, ' ');
